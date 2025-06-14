05ad4f98945f575df0e5db9ebe6bfe2745f82e62
jatindevz <jatinpatildev.z@gmail.com>
2025-06-14 14:42:40 +0530

    push

--- src/app/(app)/groups/group/page.jsx
+++ src/app/(app)/groups/group/page.jsx
@@ -2,7 +2,6 @@
 
 import React, { useEffect, useState } from 'react'
 import { Card } from '@/components/ui/card'
-import { Skeleton } from '@/components/ui/skeleton'
 import axios from 'axios'
 
 const Group = () => {
@@ -30,29 +29,7 @@ const Group = () => {
 
     return (
         <div className="flex min-h-screen bg-[#0F0F15] text-white">
-            {/* Sidebar */}
-            <div className="w-[250px] border-r border-[#252535] bg-[#1A1A25] p-4">
-                <h2 className="text-lg font-semibold mb-4">My Groups</h2>
-                <ScrollArea className="h-[calc(100vh-80px)] pr-2">
-                    {loading
-                        ? Array.from({ length: 4 }).map((_, i) => (
-                            <Skeleton key={i} className="h-10 mb-2 rounded-md bg-[#252535]" />
-                        ))
-                        : joinedGroups.length > 0
-                            ? joinedGroups.map((group) => (
-                                <div
-                                    key={group._id}
-                                    onClick={() => setSelectedGroup(group)}
-                                    className={`cursor-pointer p-2 rounded-md hover:bg-[#2a2a3d] ${selectedGroup?._id === group._id ? 'bg-[#6C4DF6]/20' : ''
-                                        }`}
-                                >
-                                    {group.name}
-                                </div>
-                            ))
-                            : <p className="text-sm text-gray-400">You haven’t joined any groups.</p>
-                    }
-                </ScrollArea>
-            </div>
+            
 
             {/* Main Content */}
             <div className="flex-1 p-6">
